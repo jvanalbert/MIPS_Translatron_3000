e46ccf0dd704fe5e3ac5cb0cffb711078c58203a
julia van albert <jv00017@mix.wvu.edu>
2025-10-26 07:29:56 -0400

    MFLO corrected

--- MFLO.c
+++ MFLO.c
@@ -37,9 +37,9 @@ void mflo_reg_assm(void) {
 	setBits_num(15, PARAM1.value, 5);
 
 	// Set the funct 
-	setBits_str(5, "010000");
+	setBits_str(5, "010010"); // originally 010000 for MFHI, changed to 010010 for MFLO
 	// set 25-16 as 0s 
-	setBits_str(10, "000000");
+	setBits_str(25, "0000000000");
 
 	// set 10-6 as 0s 
 	setBits_str(10, "00000");
@@ -53,7 +53,7 @@ void mflo_reg_bin(void) {
 	// check_bits(start_bit, bit_string) returns 0 if the bit_string matches
 	//  any x will be skipped
 	// If the manual shows (0), then the value of that bit doesnt matter
-	if (checkBits(31, "000000") != 0 || checkBits(5, "010000") != 0 || checkBits(25, "0000000000") != 0 || checkBits(10, "00000") != 0) {
+	if (checkBits(31, "000000") != 0 || checkBits(5, "010010") != 0 || checkBits(25, "0000000000") != 0 || checkBits(10, "00000") != 0) {
 		state = WRONG_COMMAND;
 		return;
 	}
